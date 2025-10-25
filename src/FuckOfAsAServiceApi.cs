ffbcd0785a057b0f034798ef5d029f28aae23fc0
huok <77536370+dee-shar@users.noreply.github.com>
2025-10-25 19:19:28 +0300

    Update and rename foaas.sh to FuckOfAsAServiceApi.cs

---
+++ src/FuckOfAsAServiceApi.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace FuckOfAsAServiceApi
+{
+    public class FuckOfAsAService
+    {
+        private readonly HttpClient httpClient;
+        private readonly string apiUrl = "https://foass.1001010.com/";
+        public FuckOfAsAService()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36");
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<string> GetVersion()
+        {
+            var response = await httpClient.GetAsync($"{apiUrl}/version");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+        public async Task<string> GetOperations()
+        {
+            var response = await httpClient.GetAsync($"{apiUrl}/operations");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+        public async Task<string> GetOperationResult(string operationName, string from)
+        {
+            var response = await httpClient.GetAsync($"{apiUrl}/{operationName}/{from}");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
